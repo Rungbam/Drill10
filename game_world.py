fee4d199789e77e5b9512665ac6aeaa2d10fa8af
Rungbam(main) <marungbam@naver.com>
2023-10-19 12:19:11 +0900

    레이어 추가

--- game_world.py
+++ game_world.py
@@ -1,11 +1,11 @@
 # 게임월드 모듈
 
 # 게임월드 표현
-world = []
+world = [ [], [] ] # 2개의 레이어
 
 # 게임월드에 객체 담기
-def add_object(o):
-    world.append(o)
+def add_object(o, depth = 0):
+    world[depth].append(o)  # 지정된 깊이의 레이어에 객체 추가
 
 
 # 게임월드 객체들을 몽땅 업데이트
